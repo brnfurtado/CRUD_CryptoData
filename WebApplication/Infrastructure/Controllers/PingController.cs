7304b172660b15781e1ccf97edcc7ab734b9610c
brnfurtado <brunofurtado@poli.ufrj.br>
2023-12-29 16:48:57 -0300

    adding pingcontroller

---
+++ WebApplication/Infrastructure/Controllers/PingController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using DomainDefinitions.Data;
+using DomainDefinitions.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.EntityFrameworkCore;
+using System.Text.Json;
+
+namespace WebApplication1.Infrastructure.Controllers
+{
+    [Route("/")]
+    [ApiController]
+    public class PingController
+    {
+        [HttpGet]
+        public async Task<IActionResult> Get()
+        {
+            return new OkResult();
+        }
+    }
+}
